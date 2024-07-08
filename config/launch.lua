09e9ac43ed67d097c1bfd7b16dbc207d713ee1ee
TongXiao-L <tongxiaoxs@outlook.com>
2024-07-09 07:56:31 +0800

    updated platform

--- config/launch.lua
+++ config/launch.lua
@@ -11,6 +11,11 @@ if platform.is_win then
         { label = "Command Prompt", args = { "cmd" } },
     }
     config.default_prog = { "pwsh", "-nologo" }
+elseif platform.is_linux or platform.is_macos then
+    config.launch_menu = {
+        { label = "Bash", args = { "bash" } }
+    }
+    config.default_prog = { "bash" }
 end
 
 return config
