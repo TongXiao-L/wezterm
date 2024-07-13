4af8ff09be8c9a0a51cfd7f32c1ced8feab7655d
TongXiao-L <tongxiaoxs@outlook.com>
2024-07-13 21:53:19 +0800

    updated font

--- config/font.lua
+++ config/font.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 return {
     font = wezterm.font_with_fallback {
         { family = "JetBrainsMono Nerd Font", weight = "DemiBold" },
-        { family = "阿里巴巴普惠体 3.0", scale = 1.1, weight = "Medium" }
+        { family = "MiSans", scale = 1.1, weight = "Medium" }
     },
     font_size = 11,
     line_height = 1.0,
